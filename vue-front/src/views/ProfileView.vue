5aa723f9bad2636399f1eb691d635c1f0f42e0db
9oogone <rhdns208@gmail.com>
2024-05-17 14:38:59 +0900

    update

---
+++ vue-front/src/views/ProfileView.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+      <h1>프로필 페이지</h1>
+      <div v-if="user">
+        <p>Username: {{ user.username }}</p>
+        <p>Nickname: {{ user.nickname }}</p>
+      </div>
+      <div v-else>
+        <p>사용자 정보가 없습니다.</p>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  import { computed, onMounted } from 'vue';
+  import { useAuthStore } from '@/stores/counter';
+  
+  const authStore = useAuthStore();
+  const user = computed(() => authStore.user);
+  
+  onMounted(() => {
+    if (!authStore.user) {
+      authStore.fetchUserDetails();
+    }
+  });
+  </script>
+  
+  <style scoped>
+  /* 추가적인 스타일 정의 가능 */
+  </style>
+  
