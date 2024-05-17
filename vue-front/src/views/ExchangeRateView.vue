43ae74eec968ccef19cb2c222843345db07fa7c4
hongseongju <moon981121@gmail.com>
2024-05-17 16:58:36 +0900

    성주

--- vue-front/src/views/ExchangeRateView.vue
+++ vue-front/src/views/ExchangeRateView.vue
@@ -1,12 +1,15 @@
 <template>
   <main>
     <h1>환율 계산기</h1>
+    <div>
+        <Exchange />        
+    </div>
   </main>
 </template>
 
 
 <script setup>
-
+import Exchange from '../components/Exchange.vue'
 </script>
 
 <style scoped>
