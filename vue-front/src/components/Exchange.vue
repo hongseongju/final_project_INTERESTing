a28f367e92ec07e3cd574e3b01d262ad55e0ac77
hongseongju <moon981121@gmail.com>
2024-05-18 22:04:28 +0900

    .

--- vue-front/src/components/Exchange.vue
+++ vue-front/src/components/Exchange.vue
@@ -1,20 +1,38 @@
 <template>
     <div>
-        <h3>환율 계산하기</h3>
+        <h3>환율 계산
+            <select v-model="selectedExchange">
+            <option :value="null">국가를 선택하세요</option>
+            <option v-for="exchange in exchanges" :value="exchange"
+            :key="exchange.id">
+            {{  exchange.cur_nm  }}
+            </option>
+        </select>
+        </h3>
+        <input type="text" v-model.number="amount" placeholder="환전할 금액">{{ selectedExchange?.cur_unit }}은 {{ calculateResult.toFixed(2) }}원(\) 입니다
+    </div>
+
+
+    <div>
+        <h3>오늘의 환율</h3>
         <ul v-if="exchanges.length">
             <li v-for="exchange in exchanges" :key="exchange.id">
-                {{ exchange }}
+                {{ exchange }} <br>
+                국가 {{ exchange.cur_nm }} | {{ exchange.deal_bas_r }}
+                <hr>
             </li>
         </ul>
     </div>
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, computed } from 'vue'
 import axios from 'axios'
 
 // 데이터 저장할 변수
 const exchanges = ref([])
+const selectedExchange = ref(null)
+const amount = ref(null)
 
 // HTTP 요청 보내고 데이터 가져오는 함수
 const ExchangeData = () => {
@@ -31,6 +49,17 @@ const ExchangeData = () => {
 onMounted(() => {
     ExchangeData()
 })
+
+// 환율 계산 함수
+const calculateResult = computed(() => {
+    if (selectedExchange.value) {
+        console.log(selectedExchange.value.id)
+        const exchangeRate = (selectedExchange.value.id === 12 || selectedExchange.value.id === 13) ? parseFloat(selectedExchange.value.ttb) / 100 : parseFloat(selectedExchange.value.ttb)
+        return amount.value * exchangeRate
+    }
+    return 0
+})
+
 </script>
 
 <style scoped>
@@ -38,4 +67,15 @@ div {
     border: 1px solid rgb(188, 255, 157);
     border-radius: 10px;
 }
+select {
+    text-align:center;
+    width:150px;
+    height:20px;
+}
+input {
+    text-align:center;
+    width:100px;
+    height:20px;
+}
+
 </style>
