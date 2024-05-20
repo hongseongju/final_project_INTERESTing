a12035529b4e1b288841ddab19b4d373d5c07936
9oogone <rhdns208@gmail.com>
2024-05-20 11:42:55 +0900

    주식선물알람

--- vue-front/src/components/CashRecharge.vue
+++ vue-front/src/components/CashRecharge.vue
@@ -29,9 +29,14 @@ export default {
       axios.post('http://localhost:8000/accounts/charge-cash/', payload)
         .then(response => {
           console.log('캐시 충전 성공!', response.data)
+          alert('캐시 충전이 완료되었습니다!\n확인 버튼을 누르면 선물하기 페이지로 이동합니다.'); // 팝업 표시
+          setTimeout(() => {
+            this.$router.push('/present'); // 1초 후 선물하기 페이지로 이동
+          }, 1000); // 3000ms = 3초
         })
         .catch(error => {
           console.error('캐시 충전 실패:', error.response ? error.response.data : error.message)
+          alert('캐시 충전 실패! 가능한 금액을 충전해 주세요.'); // 팝업 표시
         })
     }
   },
