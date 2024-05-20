f931331cf6759de8adf3582b6778613b2d12dd42
9oogone <rhdns208@gmail.com>
2024-05-20 15:07:15 +0900

    nav

--- vue-front/src/App.vue
+++ vue-front/src/App.vue
@@ -1,58 +1,57 @@
 <template>
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown link
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-      </ul>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <!-- RouterLink로 변경 -->
+      <RouterLink class="navbar-brand font-weight-bold nav-link" to="/">INTERESTing</RouterLink>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <!-- <li class="nav-item">
+            <RouterLink class="nav-link" to="/">Home</RouterLink>
+          </li> -->
+          <li class="nav-item">
+            <RouterLink class="nav-link" to="/bank_map">내 주변 은행</RouterLink>
+          </li>
+          <li class="nav-item">
+            <RouterLink class="nav-link" to="/exchange_rate">환율 계산기</RouterLink>
+          </li>
+          <li class="nav-item">
+            <RouterLink class="nav-link" to="/present">주식 선물하기</RouterLink>
+          </li>
+          <li class="nav-item">
+            <RouterLink class="nav-link" to="/interest_rate">예적금 비교</RouterLink>
+          </li>
+          <!-- <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Dropdown link
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </li> -->
+        </ul>
+        <ul class="navbar-nav">
+          <li v-if="isLogin" class="nav-item">
+            <RouterLink class="nav-link" to="/profile">Profile</RouterLink>
+          </li>
+          <li v-if="isLogin" class="nav-item">
+            <button @click="logout" class="btn btn-link nav-link">Logout</button>
+          </li>
+          <li v-else class="nav-item">
+            <RouterLink class="nav-link" to="/signup">회원가입</RouterLink>
+          </li>
+          <li v-else class="nav-item">
+            <RouterLink class="nav-link" to="/login">LogIn</RouterLink>
+          </li>
+        </ul>
+      </div>
     </div>
-  </div>
-</nav>
-
-
-
-
-
-  <div>
-    <nav>
-      <RouterLink to="/">Home</RouterLink> |
-      <RouterLink to="/bank_map">내 주변 은행</RouterLink> |
-      <RouterLink to="/exchange_rate">환율 계산기</RouterLink> |
-      <RouterLink to="/present">주식 선물하기</RouterLink> |
-      <RouterLink to="/interest_rate">예적금 비교</RouterLink> |
-      <div v-if="isLogin">
-        <RouterLink to="/profile">Profile</RouterLink> |
-        <button @click="logout">Logout</button>
-      </div>
-      <div v-else>
-        <RouterLink to="/signup">회원가입</RouterLink> |
-        <RouterLink to="/login">LogIn</RouterLink>
-      </div>
-    </nav>
-    <RouterView />
-  </div>
+  </nav>
+  <RouterView />
 </template>
 
 <script setup>
@@ -60,38 +59,30 @@ import { computed } from 'vue';
 import { useAuthStore } from '@/stores/counter';
 import { RouterLink, RouterView } from 'vue-router';
 
-
 const authStore = useAuthStore();
-
 const isLogin = computed(() => authStore.isLogin);
 
 const logout = () => {
   authStore.logout();
 };
-
-
-const links = [
-    'Dashboard',
-    'Messages',
-    'Profile',
-    'Updates',
-  ]
-
-</script>
-
-<script>
-  export default {
-    data: () => ({
-      links: [
-        'Dashboard',
-        'Messages',
-        'Profile',
-        'Updates',
-      ],
-    }),
-  }
 </script>
 
 <style scoped>
-/* 스타일 추가 가능 */
+.navbar.bg-body-tertiary {
+  --bs-tertiary-bg-rgb: 1, 65, 91; /* 원하는 RGB 값으로 변경 */
+  background-color: rgb(var(--bs-tertiary-bg-rgb)) !important;
+}
+.navbar-nav .nav-link {
+  color: #fff !important; /* 텍스트 색상 변경 */
+}
+.navbar-nav .nav-link:hover {
+  color: #CCEA8D !important; /* 링크 호버 색상 변경 */
+}
+.navbar-toggler {
+  border-color: #fff !important; /* 토글 버튼 테두리 색상 변경 */
+}
+.font-weight-bold {
+  font-weight: bold;
+  color: #fff !important; /* 텍스트 색상 변경 */
+}
 </style>
