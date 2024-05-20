2316b8bc5051f91b36596f8ecb13012495169aa3
hongseongju <moon981121@gmail.com>
2024-05-20 13:26:02 +0900

    .

--- vue-front/src/App.vue
+++ vue-front/src/App.vue
@@ -1,4 +1,40 @@
 <template>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown link
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+
+
+
   <div>
     <nav>
       <RouterLink to="/">Home</RouterLink> |
@@ -24,6 +60,7 @@ import { computed } from 'vue';
 import { useAuthStore } from '@/stores/counter';
 import { RouterLink, RouterView } from 'vue-router';
 
+
 const authStore = useAuthStore();
 
 const isLogin = computed(() => authStore.isLogin);
@@ -31,6 +68,28 @@ const isLogin = computed(() => authStore.isLogin);
 const logout = () => {
   authStore.logout();
 };
+
+
+const links = [
+    'Dashboard',
+    'Messages',
+    'Profile',
+    'Updates',
+  ]
+
+</script>
+
+<script>
+  export default {
+    data: () => ({
+      links: [
+        'Dashboard',
+        'Messages',
+        'Profile',
+        'Updates',
+      ],
+    }),
+  }
 </script>
 
 <style scoped>
--- vue-front/src/main.js
+++ vue-front/src/main.js
@@ -1,16 +1,11 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
+import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
-// Vuetify for Vue 3
-import { createVuetify } from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'
-
 // Bootstrap Vue 3
-import BootstrapVue3 from 'bootstrap-vue-3'
+import { BootstrapVue3 } from 'bootstrap-vue-3'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 
@@ -19,15 +14,11 @@ const pinia = createPinia()
 
 pinia.use(piniaPluginPersistedstate)
 
-// app.use(createPinia())
 app.use(router)
 app.use(pinia)
 
-// Vuetify setup
-const vuetify = createVuetify(/* options */)
-app.use(vuetify)
 
 // Bootstrap Vue 3 setup
-app.use(BootstrapVue3);
+app.use(BootstrapVue3)
 
 app.mount('#app')
