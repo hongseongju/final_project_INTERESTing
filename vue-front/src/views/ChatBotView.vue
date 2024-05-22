a3c99925741942e6fbef71f2ef89e036eb720b57
hongseongju <moon981121@gmail.com>
2024-05-22 22:46:20 +0900

    .

--- vue-front/src/views/ChatBotView.vue
+++ vue-front/src/views/ChatBotView.vue
@@ -1,13 +1,20 @@
 <template>
+    <div class="container">
+        <img src="`/src/assets/chatbot.png`" alt="chatbot image" class="m-2">
+        <p>무엇이든 물어보세요!</p>
+        <h1>INTERESTing 챗봇과 추천 예적금 찾기</h1>
+    </div>
     <div>
         <ChatBot />     
     </div>
 </template>
-
 
 <script setup>
 import ChatBot from '../components/ChatBot2.vue'
 </script>
 
 <style scoped>
+img {
+    width: 50px;
+}
 </style>
